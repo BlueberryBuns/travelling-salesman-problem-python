f2f143e4a1b8c73bfa4d13e67f56f3337078d9fb
tentato <alepaw99@gmail.com>
2022-10-16 18:51:18 +0200

    get-instances-for-mutation-done-not-tested

--- instance/mutations.py
+++ instance/mutations.py
@@ -7,9 +7,6 @@ from instance.solution import Solutions
 import numpy as np
 
 
-swap_rate = 0.01 # 1%
-inversion_rate = 0.1
-
 class Mutate:
     def __init__(self, solutions: Solutions) -> None:
         self.solutions = solutions
@@ -18,8 +15,10 @@ class Mutate:
         config_value = "swap"
 
         if config_value == "swap":
+            mutation_rate = 0.01
             self._swap()
         else:
+            mutation_rate = 0.1
             self._inverse()
 
     def _swap(self) -> Solutions:
@@ -28,14 +27,18 @@ class Mutate:
     def _inverse(self) -> Solutions:
         return 0
 
-
+    # It has not been tested yet!
     def _get_instances_for_mutation(self, rate: float):
+
         random_ints = np.random.randint(100, len(self.solutions.solution_array))
-        to_be_mutated = np.where(random_ints<)
+        to_be_mutated = np.where(random_ints<rate*100)
+        instances_for_mutation = self.solutions.solution_array[to_be_mutated, :]
+
+        return instances_for_mutation
 
 
 
-
+# to be deleted
 class FixtureFactory:
     @classmethod
     def create_solution(cls) -> Solutions:
