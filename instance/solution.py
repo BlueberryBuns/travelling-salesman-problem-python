9a6c024e8544470a97d555f522b69c153f92ff54
Kamil Hulewicz <48947460+BlueberryBuns@users.noreply.github.com>
2022-10-17 00:06:49 +0200

    Feat/refactor random (#8)
    * Change default configuration, alter .gitignore
    
    * Update greedy, change way of handling the data in arrays
    
    Co-authored-by: kamil.hulewicz <kamil.hulewicz@peakdata.com>

--- instance/solution.py
+++ instance/solution.py
@@ -1,13 +1,51 @@
 from functools import cached_property
+import logging
 import numpy as np
 
 
 class Solutions:
-    def __init__(self, cities, instances):
+    def __init__(self, cities: int, instances: int, init_method: str):
+        self.number_of_instances = instances
+        self.number_of_cities = cities
         self.best_solution = np.zeros((cities), dtype=np.uint16)
-        self.best_distance = np.iinfo(np.uint64).max
-        self.solution_array = np.zeros((instances, cities), dtype=np.uint16)
-        self.distance_array = np.zeros((instances, cities), dtype=np.uint64)
+        self.best_distance = np.Inf
+        self.solution_array = self._initialize_solution_array(init_method)
+        self.distance_array = np.zeros((instances, cities), dtype=float)
+
+    def _initialize_solution_array(self, method: str):
+        try:
+            method_mapping = {"random": self.random_init, "greedy": self.greedy_init}
+            return method_mapping[method](
+                self.number_of_instances, self.number_of_cities
+            )
+        except KeyError:
+            raise Exception("Unsuported init method while setting up solution array")
+
+    def random_init(self, n_instances, n_cities):
+        solution_array = np.arange(n_cities * n_instances).reshape(
+            (n_instances, n_cities)
+        )
+        solution_array %= n_cities
+        solution_array = np.random.default_rng().permuted(solution_array, axis=1)
+        return solution_array.astype(np.uint16)
+
+    def greedy_init(self, n_instances, n_cities):
+        if n_cities == n_instances:
+            logging.info(
+                "Number of instances == number of cities, performing greedy search"
+            )
+            return np.zeros(
+                (self.number_of_instances, self.number_of_cities), dtype=np.uint16
+            )
+        elif n_instances > n_cities:
+            logging.info(
+                "Number of instances > number of cities, performing greedy search, filling remaining data with random search"
+            )
+            raise NotImplementedError
+        else:
+            raise AttributeError(
+                "Number of cities cannot be lower than number of instances!"
+            )
 
     @cached_property
     def total_length(self) -> np.ndarray:
--- main.py
+++ main.py
@@ -1,40 +1,39 @@
-import logging
 from pathlib import Path
-import time
 from algorithms.greedy import GreedyAlgorithm
 from instance.representation import MatrixRepresentation
 from instance.solution import Solutions
 from loader import InstanceLoader
 from loader.config import ConfigLoader
-from logging_setup import logging_setup
 from instance import MatrixRepresentation
 from algorithms import RandomAlgorithm
 
 
 def alt_main():
-    loader = InstanceLoader()
     config = ConfigLoader("config.yaml")
-
     for selected_instance in config.selected_instances:
-
-        matrix_representation = MatrixRepresentation(loader.load(selected_instance))
-        greedy_solutions = Solutions(
-            x := matrix_representation.adjacency_matix.shape[0], instances=x
-        )  # TODO: Chage the approach with x
-        algorithm = GreedyAlgorithm(matrix_representation, greedy_solutions)
-        start = time.time_ns()
+        loader = InstanceLoader(selected_instance)
+        matrix_representation = MatrixRepresentation(loader.load())
+        # greedy_solutions = Solutions(
+        #     cities=loader.dimension, instances=loader.dimension, init_method="greedy"
+        # )
+        random_solutions = Solutions(
+            cities=loader.dimension,
+            instances=config.random_number_of_instances,
+            init_method="random",
+        )
+        # algorithm_1 = GreedyAlgorithm(matrix_representation, greedy_solutions)
+        algorithm = RandomAlgorithm(matrix_representation, random_solutions)
         algorithm.execute()
-        total = time.time_ns() - start
-        import ipdb
-
-        ipdb.set_trace()
+        random_solutions.update_best_solution()
+        print(f"{random_solutions.best_solution=}")
+        print(f"{random_solutions.best_distance=}")
 
 
 def main():
     input_file = Path("example_instances/berlin11_modified.tsp")
     loader = InstanceLoader()
     config = ConfigLoader("config.yaml")
-    
+
     # print(config._config["genetic_algorithm"])
     # print(config._genetic_algorithm)
     # print(config.genetic_crosover_method)
@@ -43,10 +42,7 @@ def main():
     print(representation.verticies_list)
 
     matrixRep = MatrixRepresentation(representation)
-    print(matrixRep.adjacency_matix)
-    # import ipdb; ipdb.set_trace()
-    # logging_setup()
-    # logging.info("Test message")
+
     result = RandomAlgorithm(representation, matrixRep).execute()
     print(result)
 
