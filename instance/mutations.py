d56c14cbb577f236afdf58b870b1be6d327ea6fd
tentato <alepaw99@gmail.com>
2022-10-16 18:44:09 +0200

    work-in-progress

--- instance/mutations.py
+++ instance/mutations.py
@@ -1,21 +1,40 @@
+from functools import cached_property
+import random
 from instance import representation
 from instance import Node
+from instance import solution
 from instance.solution import Solutions
 import numpy as np
+
+
+swap_rate = 0.01 # 1%
+inversion_rate = 0.1
 
 class Mutate:
     def __init__(self, solutions: Solutions) -> None:
         self.solutions = solutions
-        self.swap_rate = 0.01 # 1%
-        self.inversion_rate = 0.1
-        self.random = np.random.randint(100, len(solutions.solution_array))
 
-    def Swap(self) -> Solutions:
+    def mutate(self) -> Solutions:
+        config_value = "swap"
 
+        if config_value == "swap":
+            self._swap()
+        else:
+            self._inverse()
+
+    def _swap(self) -> Solutions:
         return 0
 
-    def Inverse(self) -> Solutions:
+    def _inverse(self) -> Solutions:
         return 0
+
+
+    def _get_instances_for_mutation(self, rate: float):
+        random_ints = np.random.randint(100, len(self.solutions.solution_array))
+        to_be_mutated = np.where(random_ints<)
+
+
+
 
 class FixtureFactory:
     @classmethod
