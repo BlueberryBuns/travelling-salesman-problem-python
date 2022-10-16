1a8a122821f184df0221ed93ab61db5d54aac2ab
tentato <alepaw99@gmail.com>
2022-10-16 18:21:16 +0200

    Mutate-class

---
+++ instance/mutations.py
@@ -0,0 +1,16 @@
+from instance import representation
+from instance import Node
+from instance.solution import Solutions
+import numpy as np
+
+class Mutate:
+    def __init__(self, solutions: Solutions) -> None:
+        self.solutions = solutions
+        self.mutation_rate = 0.01 # 1%
+        self.solutions_to_be_mutated = np.
+
+    def Swap(self) -> Solutions:
+        return 0
+
+    def Inverse(self) -> Solutions:
+        return 0
