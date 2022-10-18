6509a38a8d1bfb926b19e70c6a0c08c93ca135da
kamil.hulewicz <kamil.hulewicz@peakdata.com>
2022-10-18 22:19:14 +0200

    Fix inproper type casueing numba to fail

--- loader/loader.py
+++ loader/loader.py
@@ -37,7 +37,7 @@ class InstanceLoader:
                 float(splitted_line[2]),
             )
             nodes.append(node)
-        return np.array(nodes, dtype=np.float16)
+        return np.array(nodes, dtype=float)
 
     @cached_property
     def info_dict(self) -> dict:
