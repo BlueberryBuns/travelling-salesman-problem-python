90c58d0fd0c6db9e94685440bdb4334a31faf56a
kamil.hulewicz <kamil.hulewicz@peakdata.com>
2022-10-18 23:13:46 +0200

    Update mutation methods

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def alt_main():
         random_solutions = Solutions(
             cities=loader.dimension,
             instances=config.random_number_of_instances,
-            init_method="random",
+            init_method=config.genetic_init_method,
         )
         # algorithm_1 = GreedyAlgorithm(matrix_representation, greedy_solutions)
         algorithm = RandomAlgorithm(matrix_representation, random_solutions)
@@ -34,51 +34,35 @@ def alt_main():
 
 
 def main():
-    input_file = Path("example_instances/berlin11_modified.tsp")
-    loader = InstanceLoader()
     config = ConfigLoader("config.yaml")
-
-    def create_solution() -> Solutions:
-        sol = Solutions(cities=5, instances=5)
-        sol.solution_array = np.array(
-            (
-                (0, 1, 2, 3, 4),
-                (1, 4, 3, 2, 0),
-                (0, 1, 3, 2, 4),
-                (1, 0, 3, 2, 4),
-                (1, 4, 0, 2, 3),
+    for selected_instance in config.selected_instances:
+        loader = InstanceLoader(selected_instance)
+        def create_solution() -> Solutions:
+            sol = Solutions(cities=5, instances=5, init_method=config.genetic_init_method)
+            sol.solution_array = np.array(
+                (
+                    (0, 1, 2, 3, 4),
+                    (1, 4, 3, 2, 0),
+                    (0, 1, 3, 2, 4),
+                    (1, 0, 3, 2, 4),
+                    (1, 4, 0, 2, 3),
+                )
             )
-        )
-        sol.distance_array = np.array(
-            (
-                (12, 99, 510, 322, 146),
-                (10, 11, 50, 32, 16),
-                (19, 19, 3000, 322, 256),
-                (109, 101, 500, 302, 106),
-                (120, 909, 510, 322, 146),
+            sol.distance_array = np.array(
+                (
+                    (12, 99, 510, 322, 146),
+                    (10, 11, 50, 32, 16),
+                    (19, 19, 3000, 322, 256),
+                    (109, 101, 500, 302, 106),
+                    (120, 909, 510, 322, 146),
+                )
             )
-        )
-        return sol
+            return sol
 
-    solutionss = create_solution()
+        solutionss = create_solution()
 
-    mutated_solutions = Mutate(solutionss).mutate()
+        mutated_solutions = Mutate(solutionss, config.genetic_mutation_method ,config.genetic_mutation_rate).mutate()
     import ipdb; ipdb.set_trace()
-    # print(config._config)
-    # import ipdb; ipdb.set_trace()
-
-    # print(config._config["genetic_algorithm"])
-    # print(config._genetic_algorithm)
-    # print(config.genetic_crosover_method)
-
-    # representation = loader.load(input_file)
-    # print(representation.verticies_list)
-
-    # matrixRep = MatrixRepresentation(representation)
-
-    # result = RandomAlgorithm(representation, matrixRep).execute()
-    # print(result)
-
 
 if __name__ == "__main__":
     main()
