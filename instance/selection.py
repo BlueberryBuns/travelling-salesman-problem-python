2c3195da0c6151e6545cfd30f58d531193c5358b
tentato <alepaw99@gmail.com>
2022-10-19 20:37:54 +0200

    init

---
+++ instance/selection.py
@@ -0,0 +1,16 @@
+from instance.solution import Solutions
+import numpy as np
+from numba import njit
+
+
+class Selection:
+    def __init__(self, selection_method: str) -> None:
+        self.selection_method = selection_method
+
+    def select(self, solutions_array: np.ndarray) -> np.ndarray:
+        instances = None
+        return instances
+
+    def _tournament():
+        instances = None
+        return instances
