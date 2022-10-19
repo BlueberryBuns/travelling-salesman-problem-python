0916bec229e3e54f56d4f786203de5216dbfb874
tentato <alepaw99@gmail.com>
2022-10-19 22:43:22 +0200

    genetic-started

---
+++ algorithms/genetic.py
@@ -0,0 +1,40 @@
+import random
+import time
+
+from tqdm import tqdm
+
+# from instance import Node
+import numpy as np
+from numba import njit
+
+from instance.representation import MatrixRepresentation  # , Representation
+from instance.solution import Solutions
+
+from instance.selection import Selection
+from instance.mutations import Mutate
+
+
+class GeneticAlgorithm:
+    def __init__(
+        self, matrix_representation: MatrixRepresentation, solutions: Solutions
+    ):
+        self.solutions = solutions
+        self.matrix_representation = matrix_representation
+
+    def execute(self):
+        current_gen = 0
+        best_specimen = None
+        best_cost = np.Inf
+
+        #to w pętli 
+        
+        selected_solutions = Selection.select(self.solutions.solution_array, self.solutions.distance_array)
+
+        # population = Crossovers.crossover(selected_solutions)
+
+        population = Mutate.mutate(population)
+
+        # evaluation
+
+        current_gen += 1
+        return best_cost, best_specimen
